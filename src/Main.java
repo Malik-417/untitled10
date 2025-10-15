1e40da06a64e13f75a535df06a6bc56b825a302e
Malik <malik200696@yandex.ru>
2025-10-15 03:48:23 +0300

    123

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main (String[] ards){
+        System.out.println("Task 1");
+        // Создаем переменные для хранения ФИО
+        String frisName = "Человек";
+        String middleName = "Человекович";
+        String lastName = "Человеков";
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        // полное ФИО
+        String fullName = lastName + " " + frisName + " " + middleName;
+
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
     }
 }
